39c71ad164979cc4848850f55ae9da5b801b2f25
Hidden <hiddengob@free.fr>
2020-05-24 15:28:41 +0200

    First commit

---
+++ database/models.py
@@ -0,0 +1,55 @@
+from .db import db
+from flask_bcrypt import generate_password_hash, check_password_hash
+
+
+class Skill(db.Document):
+    name = db.StringField(required=True, primary_key=True)
+    domaines = db.ListField(db.StringField(choices=["basique",
+                                                    "arts et artisanats",
+                                                    "sciences du combat",
+                                                    "developpement corporel",
+                                                    "sciences et humanités",
+                                                    "occulte et religions",
+                                                    "sciences et techniques",
+                                                    "don"]), required=True)
+    description = db.StringField(required=True)
+    category = db.StringField(choices=["general", "specialisation", "don", "passion"], required=True)
+    optional_mechanism = db.DynamicField()
+
+
+class User(db.Document):
+    email = db.EmailField(required=True, unique=True)
+    password = db.StringField(required=True, min_length=6)
+    role = db.StringField(choices=("Admin", "GM", "Player", "Writer"), default="Player")
+
+    def hash_password(self):
+        self.password = generate_password_hash(self.password).decode('utf8')
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+
+class Culture(db.Document):
+    name = db.StringField(required=True, primary_key=True)
+    description = db.StringField(required=True)
+    gabarit = db.IntField(required=True)
+    innate = db.ListField(db.StringField(), required=True)
+    gift = db.ListField(db.StringField())
+
+
+class Childhood(db.Document):
+    name = db.StringField(required=True, primary_key=True)
+    description = db.StringField(required=True)
+    innate = db.ListField(db.StringField(), required=True)
+
+
+class Career(db.Document):
+    name = db.StringField(required=True, primary_key=True)
+    description = db.StringField(required=True)
+    skills = db.ListField(db.StringField(), required=True)
+    domaine = db.StringField(required=True)
+    dotation = db.ListField(db.StringField())
+
+
+class Character(db.Document):
+    pass
---
+++ resources/childhood.py
@@ -0,0 +1,83 @@
+from json import load, dumps
+
+from flask import Response
+from flask_restful import Resource
+
+from database.models import Childhood
+
+
+class ChildhoodAPI(Resource):
+    def get(self, name):
+        childhood = Childhood.objects.get(name=name).to_json()
+        return Response(response=childhood, status=200, content_type="application/json")
+
+
+class ChildhoodsApiList(Resource):
+    def get(self):
+        """
+        Retrieve all existing childhoods
+        ---
+        tags:
+            - childhood
+        definitions:
+            childhoods_name:
+                type: array
+                items:
+                    $ref: '#/definitions/basic-ref'
+        responses:
+            200:
+                description: "The list of childhood'names"
+                schema:
+                    $ref: '#/definitions/childhoods_name'
+        """
+        childhoods = Childhood.objects.only("name")
+        names = [item["name"] for item in childhoods]
+        return Response(response=dumps(names), status=200)
+
+
+class ChildhoodsApiFull(Resource):
+    def get(self):
+        """
+        Retrieve all childhoods data
+        ---
+        tags:
+            - childhood
+        definitions:
+            childhoods:
+                type: array
+                items:
+                    $ref: '#/definitions/childhood'
+            childhood:
+                type: object
+                properties:
+                    name:
+                        type: string
+                        required: true
+                    description:
+                        type: string
+                        required: true
+                    innate:
+                        type: array
+                        items:
+                            $ref: '#/definitions/basic-ref'
+            basic-ref:
+                type: string
+
+        responses:
+            200:
+                description: "The childhoods full data"
+                schema:
+                    $ref: '#/definitions/childhoods'
+
+        """
+
+        childhoods = Childhood.objects().to_json()
+        return Response(response=childhoods, status=200)
+
+
+def initialize_childhood():
+    with open("childhood.json") as file:
+        childhoods = load(file)
+        for childhood in childhoods:
+            print(childhood["name"])
+            Childhood(**childhood).update(**childhood, upsert=True)
